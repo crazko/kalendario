ab83e9b76901df69e75c3b644f5f638cd261367a
craz <crazko@gmail.com>
2017-10-29 20:30:17 +0100

    init

---
+++ README.md
@@ -0,0 +1,45 @@
+# Calex
+A browser extension that shows events' descriptions in Google Calendar.
+
+Descriptions of the events are displayed in the agenda and search view.
+
+## Installation
+Install from [Chrome Web Store](https://chrome.google.com/webstore/detail/calex-for-google-calendar/ccoehijdbponhcemihobmdpaeenmgchg).
+![Calex in Google Calendar](docs/screenshot.png)
+
+## Features
+
+- shows descriptions in:
+    - the agenda view
+    - the search results
+- supports markdown format thanks to [showdownjs/showdown](https://github.com/showdownjs/showdown)
+
+### Browsers
+Tested in:
+
+- [Chrome](https://www.google.com/chrome/)
+- [Opera](https://www.opera.com/) (you can use Chrome extensions trough [Download Chrome Extensions](https://addons.opera.com/en/extensions/details/download-chrome-extension-9/) addon)
+- [Vivaldi](https://vivaldi.com/)
+
+## Development
+
+- clone this repository
+- checkout a new branch
+- load directory as an unpacked extension (be sure you have enabled **developer mode**)
+- you have to reload extension on every change you made
+
+### Workflow:
+
+1. get list of user calendars
+    - parse calendars' ids
+2. get all events from the page
+    - their ids
+    - calendar names
+3. get event description
+    - get calendar id from the list by its name
+    - use event id and calendar id to get description
+
+### API calls
+
+- [CalendarList: list](https://developers.google.com/google-apps/calendar/v3/reference/calendarList/list)
+-
---
+++ assets/js/content.js
@@ -0,0 +1,75 @@
+/**
+ * Adds description to given event.
+ * @param {string} eventId
+ * @param {string} description
+ */
+const addDescription = function(eventId, description) {
+	let $event = $('#' + eventId);
+
+  // Avoid duplicated content
+  if ($event.find('.calex__description')) {
+    $event.find('.calex__description').remove();
+  }
+
+  $event.append(`<div class="calex__description">${description}</div>`);
+}
+
+/**
+ * Gets all events from calendar.
+ */
+const getAllEvents = () => {
+  const rows = document.querySelectorAll('div[role="row"]');
+
+  return Array.from(rows)
+    .filter(row => row.children[1] && row.children[1].children.length === 3)
+    .map(event => {
+      let eventid = atob(event.children[1].children[1].children[0].dataset.eventid);
+      eventid = eventid.split(' ');
+
+      return {
+        id: eventid[0],
+        calendarName: event.children[1].children[2].children[0].dataset.text
+      }
+  });
+}
+
+/* ----------------------------------------------------- */
+
+const calendarContainer = document.querySelector('header[role="banner"]');
+// const calendarContainer = document.querySelector('div[role="main"]');
+const markdownConverter = new showdown.Converter({
+  noHeaderId: true,
+  simplifiedAutoLink: true,
+  excludeTrailingPunctuationFromURLs: true,
+  strikethrough: true
+});
+
+// Check for change in calendar.
+// Gather all events and send them to background to process
+const observer = new MutationObserver(function(mutations) {
+
+  if (document.querySelector('div[role="grid"]') !== null) {
+    chrome.runtime.sendMessage({
+      msg: 'events',
+      events: getAllEvents()
+    }, function(response) {
+      // console.log(response);
+    });
+  };
+});
+
+observer.observe(calendarContainer, {
+  childList: true,
+  characterData: true,
+  subtree: true
+});
+
+// Wait for processed events and paste their data
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+  if (request.msg === 'show') {
+    let event = request.event;
+    // let html = markdownConverter.makeHtml(event.description);
+    console.log(event.description)
+    // addDescription(event.id, html);
+  }
+});
